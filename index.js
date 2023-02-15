3261ef46611676e2f85a80844cd191bdc5666181
ganesh077 <ganeshthampi077@gmail.com>
2023-02-15 13:58:07 -0500

    new changes

--- index.js
+++ index.js
@@ -1,53 +1,41 @@
 const express = require('express');
-const AWS = require('aws-sdk');
-const uuid = require('uuid');
 const app = express();
 const port = 3000;
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+AWS.config.update({
+  region: 'us-west-2',
+});
+
+const s3 = new AWS.S3();
+
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
 
-// Set up AWS credentials and region
-const s3 = new AWS.S3({ apiVersion: '2006-03-01' });
-
-// Set up S3 bucket name
-const bucketName = 'mediasharingapp';
-
-// Route to retrieve all items from S3 bucket and render them in the web page
 app.get('/', (req, res) => {
-  s3.listObjectsV2({ Bucket: bucketName }, (err, data) => {
+  const params = { Bucket: 'your-bucket-name', MaxKeys: 100 };
+  s3.listObjectsV2(params, function (err, data) {
     if (err) {
-      console.log(`Error getting objects from S3 bucket: ${err}`);
-      res.status(500).send('Error getting objects from S3 bucket');
+      console.log(err, err.stack);
     } else {
-      let images = data.Contents.map(obj => {
-        let key = obj.Key;
-        let url = s3.getSignedUrl('getObject', { Bucket: bucketName, Key: key });
-        return { key: key, url: url };
+      const images = data.Contents.filter(function (item) {
+        return item.Key.match(/\.(jpg|jpeg|png|gif)$/);
       });
       res.render('index', { images: images });
     }
   });
 });
 
-// Route to render form to upload new image
 app.get('/new', (req, res) => {
   res.render('new');
 });
 
-// Route to handle form submission and upload new image to S3 bucket
 app.post('/create', (req, res) => {
-  let file = req.files.image;
-  let key = uuid.v4() + '-' + file.name;
-  let params = { Bucket: bucketName, Key: key, Body: file.data };
-  s3.upload(params, (err, data) => {
-    if (err) {
-      console.log(`Error uploading object to S3 bucket: ${err}`);
-      res.status(500).send('Error uploading object to S3 bucket');
-    } else {
-      console.log(`Object uploaded to S3 bucket: ${data.Location}`);
-      res.redirect('/');
-    }
-  });
+  req.body.EIB = uuid.v4();
+  // Add code to save data to your database
+  res.redirect('/');
 });
 
 app.listen(port, () => {
